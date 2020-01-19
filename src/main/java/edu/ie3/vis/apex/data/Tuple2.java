6e3b49cc2fd95dd6196b9fe58c523d1e04c9dffd
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2020-01-19 20:50:25 +0100

    Tuple2 documentation

--- src/main/java/edu/ie3/vis/apex/data/Tuple2.java
+++ src/main/java/edu/ie3/vis/apex/data/Tuple2.java
@@ -8,47 +8,53 @@ package edu.ie3.vis.apex.data;
 
 import java.util.Objects;
 
+
 /**
- * //ToDo: Class Description
+ * Simple representation of a (x,y) Tuple with two elements
  *
- *
- * @version 0.1
- * @since 13.01.20
+ * @param <X> type of x element, can be e.g. date strings or numerical values
+ * @param <Y> type of y element, normally numeric values
  */
-public class Tuple2<X extends Comparable<X>, Y extends Comparable<Y>>
-    implements Comparable<Tuple2<X, Y>> {
+public class Tuple2<X extends Comparable<X>, Y extends Comparable<Y>> implements Comparable<Tuple2<X, Y>> {
 
-  private final X x;
-  private final Y y;
+    private final X x;
+    private final Y y;
 
-  public Tuple2(X x, Y y) {
-    this.x = x;
-    this.y = y;
-  }
+    /**
+     * Create a (x,y) tuple
+     * @param x
+     * @param y
+     */
+    public Tuple2(X x, Y y) {
+        this.x = x;
+        this.y = y;
+    }
 
-  public X getX() {
-    return x;
-  }
+    public X getX() {
+        return x;
+    }
 
-  public Y getY() {
-    return y;
-  }
+    public Y getY() {
+        return y;
+    }
 
-  @Override
-  public int compareTo(Tuple2<X, Y> other) {
-    return this.x.compareTo(other.x);
-  }
+    @Override
+    public int compareTo(Tuple2<X, Y> other) {
+        return this.x.compareTo(other.x);
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    Tuple2<?, ?> tuple2 = (Tuple2<?, ?>) o;
-    return x.equals(tuple2.x) && y.equals(tuple2.y);
-  }
+    @Override
+    public boolean equals(Object o) {
+        if(this == o)
+            return true;
+        if(o == null || getClass() != o.getClass())
+            return false;
+        Tuple2<?, ?> tuple2 = (Tuple2<?, ?>) o;
+        return x.equals(tuple2.x) && y.equals(tuple2.y);
+    }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(x, y);
-  }
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 }
